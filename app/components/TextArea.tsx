281b4afc3df5e188d5d8fda5d50da08e57f3b7cb
Sunil Pai <spai@cloudflare.com>
2024-06-13 20:17:12 +0100

    pass the ref to the textarea

--- app/components/TextArea.tsx
+++ app/components/TextArea.tsx
@@ -4,8 +4,9 @@ import { cn } from '~/utils/style'
 export const TextArea = forwardRef<
 	HTMLTextAreaElement,
 	JSX.IntrinsicElements['textarea']
->(({ className, ...rest }, _ref) => (
+>(({ className, ...rest }, ref) => (
 	<textarea
+		ref={ref}
 		className={cn(
 			'bg-zinc-50 dark:bg-zinc-600 text-base border-2 border-zinc-500 w-full resize-none block p-2 rounded',
 			className
