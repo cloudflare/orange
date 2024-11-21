af4e80ede1f48b0f8a9da3c910632729a3fac392
Michael Rosenberg <mrosenberg@cloudflare.com>
2024-11-21 18:37:21 -0500

    TEMP: check in MLS worker build artifacts

---
+++ public/e2ee/wasm-pkg/orange_mls_worker_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function initLogging(): void;
+export function processEvent(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h50aea8050d8e200e(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h88f76befb6b77aae(a: number, b: number, c: number, d: number): void;
