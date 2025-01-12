9463a65099010f00060ee0128aae271d93cdf0bc
Michael Rosenberg <mrosenberg@cloudflare.com>
2025-01-11 22:48:28 -0500

    MLS: Removed cargo dep patches

--- public/e2ee/wasm-pkg/orange_mls_worker.d.ts
+++ public/e2ee/wasm-pkg/orange_mls_worker.d.ts
@@ -2,15 +2,13 @@ declare namespace wasm_bindgen {
 	/* tslint:disable */
 	/* eslint-disable */
 	/**
-	* Sets some logging globals
-	*/
+	 * Sets some logging globals
+	 */
 	export function initLogging(): void;
 	/**
-	* Processes an event and returns an object that's null, i.e., no return value, or consists of
-	* fields "type": str, "payload_name": str, and "payload": ArrayBuffer.
-	* @param {object} event
-	* @returns {Promise<any>}
-	*/
+	 * Processes an event and returns an object that's null, i.e., no return value, or consists of
+	 * fields "type": str, "payload_name": str, and "payload": ArrayBuffer.
+	 */
 	export function processEvent(event: object): Promise<any>;
 	
 }
@@ -20,14 +18,17 @@ declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssemb
 declare interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly initLogging: () => void;
-  readonly processEvent: (a: number) => number;
+  readonly processEvent: (a: any) => any;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-  readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha2fe9d6d9cecb9bd: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_exn_store: (a: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke2_mut__hd439513aaedacbc3: (a: number, b: number, c: number, d: number) => void;
+  readonly __wbindgen_export_6: WebAssembly.Table;
+  readonly closure408_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure478_externref_shim: (a: number, b: number, c: any, d: any) => void;
+  readonly __wbindgen_start: () => void;
 }
 
 /**
