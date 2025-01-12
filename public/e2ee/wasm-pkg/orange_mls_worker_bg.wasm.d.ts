34b085c9a0dd8647859952928a93a870d0eed59c
Michael Rosenberg <mrosenberg@cloudflare.com>
2025-01-11 22:48:28 -0500

    MLS: Removed cargo dep patches

--- public/e2ee/wasm-pkg/orange_mls_worker_bg.wasm.d.ts
+++ public/e2ee/wasm-pkg/orange_mls_worker_bg.wasm.d.ts
@@ -1,12 +1,15 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function initLogging(): void;
-export function processEvent(a: number): number;
-export function __wbindgen_malloc(a: number, b: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const initLogging: () => void;
+export const processEvent: (a: any) => any;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__ha2fe9d6d9cecb9bd(a: number, b: number, c: number): void;
-export function __wbindgen_free(a: number, b: number, c: number): void;
-export function __wbindgen_exn_store(a: number): void;
-export function wasm_bindgen__convert__closures__invoke2_mut__hd439513aaedacbc3(a: number, b: number, c: number, d: number): void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure408_externref_shim: (a: number, b: number, c: any) => void;
+export const closure478_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const __wbindgen_start: () => void;
