7c7d9db2dacc4aec30936ba28c950148616aa559
Kevin Kipp <kkipp@cloudflare.com>
2025-07-01 11:05:22 -0500

    Revert "Rename api base url"
    This reverts commit 32c7c14e019f60e1a86e54df013136ba73f71f05.

--- app/routes/partytracks.$.tsx
+++ app/routes/partytracks.$.tsx
@@ -5,7 +5,7 @@ const proxy = async ({ request, context }: LoaderFunctionArgs) =>
 	routePartyTracksRequest({
 		appId: context.env.CALLS_APP_ID,
 		token: context.env.CALLS_APP_SECRET,
-		realtimeApiBaseUrl: context.env.CALLS_API_URL,
+		callsApiBaseUrl: context.env.CALLS_API_URL,
 		request,
 	})
 
